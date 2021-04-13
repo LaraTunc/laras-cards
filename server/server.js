20d8040f2bfa55c9d21a498fb65366730141b8a0
Lara Tunc <laratunc@gmail.com>
2021-04-13 14:11:46 -0400

    changes for heroku

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 var app = express();
 
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(require('./routes'));
+app.use(morgan("dev"));
+app.use(require("./routes"));
 
 const server = app.listen(PORT, function () {
-    console.info('🌍 Listening on port ' + server.address().port);
+  console.info("🌍 Listening on port " + server.address().port);
 });
